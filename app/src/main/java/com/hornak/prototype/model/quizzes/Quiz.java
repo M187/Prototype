8bd473311f5511b5fe0100d0193494b86a4a4e14
M187 <dony66@gmail.com>
2017-11-28 11:46:42 +0100

    Model Parcelable

--- app/src/main/java/com/hornak/prototype/model/quizzes/Quiz.java
+++ app/src/main/java/com/hornak/prototype/model/quizzes/Quiz.java
@@ -1,13 +1,28 @@
 package com.hornak.prototype.model.quizzes;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.List;
 
 /**
  * Created by michal.hornak on 11/23/2017.
  */
 
-public class Quiz {
+public class Quiz implements Parcelable {
 
+    // Parcelling part
+    public static final Creator<Quiz> CREATOR = new Creator<Quiz>() {
+        @Override
+        public Quiz createFromParcel(Parcel in) {
+            return new Quiz(in);
+        }
+
+        @Override
+        public Quiz[] newArray(int size) {
+            return new Quiz[size];
+        }
+    };
     public String name;
     public String timestamp;
     public List<Team> teams;
@@ -18,6 +33,12 @@ public class Quiz {
         this.name = name;
         this.timestamp = timestamp;
         this.teams = teams;
+    }
+
+    public Quiz(Parcel in){
+        this.name = in.readString();
+        this.timestamp = in.readString();
+        in.readTypedList(teams, Team.CREATOR);
     }
 
     public String getName() {
@@ -32,4 +53,15 @@ public class Quiz {
         return teams;
     }
 
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.name);
+        dest.writeString(this.timestamp);
+        dest.writeTypedList(this.teams);
+    }
 }
--- app/src/main/java/com/hornak/prototype/model/quizzes/Team.java
+++ app/src/main/java/com/hornak/prototype/model/quizzes/Team.java
@@ -1,26 +1,57 @@
 package com.hornak.prototype.model.quizzes;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by michal.hornak on 11/23/2017.
  */
 
-public class Team {
+public class Team implements Parcelable {
 
+    // Parcelling part
+    public static final Creator<Team> CREATOR = new Creator<Team>() {
+        @Override
+        public Team createFromParcel(Parcel in) {
+            return new Team(in);
+        }
+
+        @Override
+        public Team[] newArray(int size) {
+            return new Team[size];
+        }
+    };
     public String name;
     public String pointsAchieved;
 
 public Team(){}
 
-    public Team(String name, String pointsAchieved){
+        public Team(String name, String pointsAchieved){
         this.name = name;
         this.pointsAchieved = pointsAchieved;
+    };
+
+    public Team(Parcel in){
+        this.name = in.readString();
+        this.pointsAchieved =  in.readString();
     }
 
-        public String getName() {
+public String getName() {
         return name;
-    };
+    }
 
     public String getPointsAchieved() {
         return pointsAchieved;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.name);
+        dest.writeString(this.pointsAchieved);
     }
 }
